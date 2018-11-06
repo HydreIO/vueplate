4bfba0a09d796065932f67effe711a532da28268
Sceat <11330271+Sceat@users.noreply.github.com>
2018-11-06 22:01:46 +0200

    few fix

--- src/main.js
+++ src/main.js
@@ -1,31 +1,31 @@
 import Vue from 'vue'
-import App from '@/App.vue'
+import App from '@/sidy.vue'
 import router from '@/router'
 import VueMq from 'vue-mq'
 import Ripple from 'vue-ripple-directive'
+import VueWaypoint from 'vue-waypoint'
 import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock'
 import store from '@/store'
 import './registerServiceWorker'
-import loading from '@cmp/loading.vue'
-import error from '@cmp/loading.vue'
 
 Vue.config.productionTip = false
 
 Vue.directive('rp', Ripple)
+Vue.use(VueWaypoint)
 Vue.use(VueMq, {
 	breakpoints: {
-		sm: 800,
+		sm: 700,
 		lg: Infinity,
 	},
 })
 
 new Vue({
-	router,
+  router,
 	methods: {
 		// litle npm script to disable body scroll on all devices (because those IOS suckers think different)
 		lockScroll: el => disableBodyScroll(el),
 		unlockScroll: el => enableBodyScroll(el),
 	},
-	store,
-	render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
