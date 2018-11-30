7a4d9fe0798f2897e87360a1afae37bd2d473dc2
Sceat <11330271+Sceat@users.noreply.github.com>
2018-12-01 00:47:35 +0200

    deprecate palette

--- src/views/misc/404.vue
+++ src/views/misc/404.vue
@@ -12,87 +12,86 @@ import Component from 'vue-class-component'
 
 @Component
 export default class e404 extends Vue {
-    deltaX = 0
-    deltaY = 0
+  deltaX = 0
+  deltaY = 0
 
-    onMove({ screenX, screenY }) {
-        if (this.lastX !== undefined && this.lastY !== undefined) {
-            this.deltaX += (this.lastX - screenX) * 0.03
-            this.deltaY += (this.lastY - screenY) * 0.03
-        }
-        this.lastX = screenX
-        this.lastY = screenY
+  onMove({ screenX, screenY }) {
+    if (this.lastX !== undefined && this.lastY !== undefined) {
+      this.deltaX += (this.lastX - screenX) * 0.03
+      this.deltaY += (this.lastY - screenY) * 0.03
     }
+    this.lastX = screenX
+    this.lastY = screenY
+  }
 
-    mounted() {
-        window.addEventListener('mousemove', this.onMove,{passive: true})
-    }
+  mounted() {
+    window.addEventListener('mousemove', this.onMove, { passive: true })
+  }
 
-    beforeDestroy() {
-        window.removeEventListener('mousemove', this.onMove)
-    }
+  beforeDestroy() {
+    window.removeEventListener('mousemove', this.onMove)
+  }
 }
 </script>
 
 
 <style lang="stylus" scoped>
-@require '~@stl/palette'
 @require '~@stl/material'
 
 .container
-    transition font-size 300ms
-    font-size .6rem
-    display grid
-    place-content center center
-    text-align center
-    background palette(1)
-    width 100%
-    height 100vh
-    grid 'sign_lg sign' max-content '. nt' max-content '. nf' max-content '. btn' max-content / max-content max-content
-    grid-column-gap 1em
-    text-transform uppercase
-    color palette(3)
+  transition font-size 300ms
+  font-size .6rem
+  display grid
+  place-content center center
+  text-align center
+  background #F5F5F5
+  width 100%
+  height 100vh
+  grid 'sign_lg sign' max-content '. nt' max-content '. nf' max-content '. btn' max-content / max-content max-content
+  grid-column-gap 1em
+  text-transform uppercase
+  color #424242
+
+  &.lg
+    font-size 1rem
+
+  :first-child
+    font-size 8em
+    color #1565C0
+    grid-area sign
+    place-self start start
 
     &.lg
-        font-size 1rem
+      grid-area sign_lg
 
-    :first-child
-        font-size 8em
-        color palette(2)
-        grid-area sign
-        place-self start start
+  :nth-child(2)
+    font-size 8em
+    font-weight 800
+    grid-area nt
+    place-self center start
 
-        &.lg
-            grid-area sign_lg
+  :nth-child(3)
+    font-size 1.5em
+    grid-area nf
+    place-self start start
+    margin-bottom 1em
+    text-align start
 
-    :nth-child(2)
-        font-size 8em
-        font-weight 800
-        grid-area nt
-        place-self center start
+  :nth-child(4)
+    width 100%
+    font-size 1.2em
 
-    :nth-child(3)
-        font-size 1.5em
-        grid-area nf
-        place-self start start
-        margin-bottom 1em
-        text-align start
+    &.lg
+      width fit-content
+      font-size 1em
 
-    :nth-child(4)
-        width 100%
-        font-size 1.2em
-
-        &.lg
-            width fit-content
-            font-size 1em
-
-        grid-area btn
-        place-self start end
-        border 1px solid palette(2)
-        border-radius 1px
-        padding .6em 1.5em
-        font-weight 400
-        text-decoration none
-        color palette(3)
-        material(1)
+    grid-area btn
+    place-self start end
+    border 1px solid #1565C0
+    border-radius 1px
+    padding .6em 1.5em
+    font-weight 400
+    text-decoration none
+    color #424242
+    material(1)
 </style>
